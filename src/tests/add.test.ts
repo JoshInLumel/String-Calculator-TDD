d305f0ff0fcf811d44654b9fc27843e919a51557
JoshInLumel <JhosiahF@lumel.com>
2025-01-08 12:09:13 +0530

    diff delimiters test case implementation

--- src/tests/add.test.ts
+++ src/tests/add.test.ts
@@ -20,4 +20,15 @@ describe("Add Operation", () => {
   test("assumes numbers greater than thousand as zero in a given string", () => {
     expect(add("2%%%1001")).toBe(2);
   });
+  test("adds the comma-separated numbers in a given string", () => {
+    expect(add("1,2")).toBe(3);
+  });
+  test("adds the numbers separated by a combination of newline and comma delimiters in the given string", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+  test("adds the numbers separated by different delimiters in the given string", () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//[*][%]\n1*2%3")).toBe(6);
+    expect(add("/[***]\n1***2***3")).toBe(6);
+  });
 });
